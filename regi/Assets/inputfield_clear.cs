32b17d434d1b6efa0abde2b8c8d6be1919498a7a
ちず <56035752+chizu1819@users.noreply.github.com>
2022-08-22 21:44:40 +0900

    修正

---
+++ regi/Assets/inputfield_clear.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+
+public class inputfield_clear : MonoBehaviour
+{
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+ 
+    public void GetText()
+    {
+        //InputField コンポーネントを取得
+        InputField form = GameObject.Find("InputField").GetComponent<InputField>();
+        form.text = "";
+        
+        InputField form_1 = GameObject.Find("InputField (1)").GetComponent<InputField>();
+        form_1.text = "";
+
+        InputField form_2 = GameObject.Find("InputField (2)").GetComponent<InputField>();
+        form_2.text = "";
+
+        InputField form_3 = GameObject.Find("InputField (3)").GetComponent<InputField>();
+        form_3.text = "";
+    }
+    
+ 
+ 
+}
